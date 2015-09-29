485acfe7540f3e588d7994e5a4834281003e7576
Fridrikv10 <fridrikv10@ru.is>
2015-09-29 13:49:05 +0000

    Add project

---
+++ Prufa/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prufa.MvcApplication" Language="C#" %>
---
+++ Prufa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prufa.Startup))]
+namespace Prufa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
